cdf581cdcdfe08827387dda8897579418206a37e
MT2314 <39105763+MT2314@users.noreply.github.com>
2020-11-29 17:20:06 -0500

    Intro, Begun table Work

--- index.js
+++ index.js
@@ -13,117 +13,198 @@ const util = require('util');
 // fs writeFile is file system file which writes data to a file
 const writeFileAsync = util.promisify(fs.writeFile);
 
-const userInput = () =>
+const userInput1 = () =>
     inquirer.prompt([
+
+        // Project Name
         {
             type: "input",
             name: "projectName",
             message: "What is the name of your project?",
         },
-        {
-            type: "input",
-            name: "email",
-            message: "What is your email address?",
-        },
-        {
-            type: "input",
-            name: "phonenumber",
-            message: "What is your phonenumber?",
-        },
-        {
-            type: "input",
-            name: "location",
-            message: "What is your location?",
-        },
-        {
-            type: "input",
-            name: "linkedin",
-            message: "What is your linkedin?",
-        },
+        // Github Repo Link
         {
             type: "input",
             name: "github",
-            message: "What is your github?",
+            message: "What is the github repository address?",
+        },
+        // Project Description
+        {
+            type: "input",
+            name: "description",
+            message: "How would you describe the project",
         },
     ]);
 
 const readMeGen = (user) =>
-
-    `# ${user.projectName}
-
+    `
     :part_alternation_mark:
     
-    # **Personal Portfolio**
-    
+    # ${user.projectName}
     ---
     <br />
-    <br />
     
-    ## Site Description
+    >Link to github repository ${user.github}
+    
+    <img src="assets/Images/websiteScreenshot.png" alt="alt text" width="600"/>
+    
+    ## Job Requirements
     ---
     
-    
-    > Personal Portfolio
-    > A list of all my best projects, a bit about me and a way to contact me
-    > Enjoy
-      
     </br>
     
-    * Functional, deployed application :heavy_check_mark:
-    
-    * GitHub repository with README describing the project :heavy_check_mark:
-    
-    * Navbar must be consistent on each page. :heavy_check_mark:
-    
-    * Navbar on each page must contain links to Home/About, Contact, and Portfolio pages. :heavy_check_mark:
-    
-    * All links must work. :heavy_check_mark:
-    
-    * Must use semantic html. :heavy_check_mark:
-    
-    * Each page must have valid and correct HTML. (use a validation service) :heavy_check_mark:
-    
-    * Must contain your personalized information. (bio, name, images, links to social media, etc.) :heavy_check_mark:
-    
-    * Must properly utilize Bootstrap components and grid system. :heavy_check_mark:
-    
-    
-    
-    </br>
-    </br>
-    
-    
-    ## Credits
-    ---
-    :school: 
-    **University of Toronto Continuing Educationg
-    Full Stack Flex Program** - *Initial HTML and CSS*
-    :school:
-    
-    :heart: 
-    **Students and Instructors At the UFT-FSF-2020-2021*
-    :heart:
-    </br>
-    
-    ## License
-    ---
-    
-    All rights reserved.
-    
-    Licensed under the MIT license.
-    <br>
-    [![MIT License](https://img.shields.io/apm/l/atomic-design-ui.svg?)](https://github.com/tterb/atomic-design-ui/blob/master/LICENSEs)
-    </br>
-    
-    :lock::lock::lock::lock:
-    
-    _____
-    ____
-    ___`;
+    > ${user.description}`;
 
-
-
-userInput()
+userInput1()
     .then((user) => writeFileAsync("README.md", readMeGen(user)))
+    .then(() => {
+        // App's abilities breakdown
+        const table = () => {
+
+            inquirer.prompt([
+                // Do you want a Data Table?
+                // //
+                {
+                    type: "confirm",
+                    name: "table",
+                    message: "Do you want to build a table?",
+                },
+            ]);
+            table()
+                .then((choice) => {
+                    const tableBuild = (choice) => {
+                        if (choice === true) {
+                            inquirer.prompt([
+                                // How many Columns?
+                                {
+                                    type: "input",
+                                    name: "columns",
+                                    message: "How many Columns",
+                                },
+
+                                // How many Rows?
+                                {
+                                    type: "input",
+                                    name: "rows",
+                                    message: "How many rows",
+                                },
+                            ])
+                        }
+                    };
+                });
+        };
+    });
+table()
+    .then(console.log("hi"))
+
+
+
     .then(() => console.log('Successfully wrote to README.md'))
     .catch((err) => console.log(err));
+
+
+
+// const userInputTable = () => {
+    //     return prompt([{
+        //         ...question,
+        //         name: 'question'
+        //     }]).then(({ question }) => question)
+        // }
+
+
+
+        // .then((user) => tableBuild(user));
+        // const tableBuild()
+        //     .
+        //     if(user.table == true){
+
+            //  * /
+            //     // Project Name
+            //     {
+                //         type: "input",
+                //             name: "projectName",
+                //                 message: "What is the name of your project?",
+                //     },
+                //     // Github Repo Link
+                //     {
+                    //         type: "input",
+                    //             name: "github",
+                    //                 message: "What is the github repository address?",
+                    //     },
+                    //     // Project Description
+                    //     {
+                        //         type: "input",
+                        //             name: "description",
+                        //                 message: "How would you describe the project",
+                        //     },
+
+
+                        //     // {
+                            //     //     type: "input",
+                            //     //     name: "location",
+                            //     //     message: "What is your location?",
+                            //     // },
+                            //     // {
+                                //     //     type: "input",
+                                //     //     name: "linkedin",
+                                //     //     message: "What is your linkedin?",
+                                //     // },
+                                // ]);
+
+                                //     const readMeGen = (user) =>
+
+                                //         `   
+
+                                // </br>
+                                // | Desires                              | Solutions                                                                                       |
+                                // | ---------------------------------------------- | ----------------------------------------------------------------------------------------------- |
+                                // `
+
+                                //             `
+                                // | WHEN I click the start button                   | THEN a timer starts and I am presented with a question   :heavy_check_mark:                                                          |
+                                // | WHEN I answer a question | THEN I am presented with another question:heavy_check_mark: |
+                                // | WHEN I answer a question incorrectly                 | THEN time is subtracted from the clock:heavy_check_mark:                                                           |
+                                // | WHEN all questions are answered or the timer reaches 0             | THEN the game is over:heavy_check_mark:                                                              |
+                                // | WHEN the game is over                  | THEN I can save my initials and score:heavy_check_mark:                                                        |
+                                // | Extra:heavy_check_mark:               | Extra:heavy_check_mark:                                                              |
+                                // | When I submit Intials                 | Then I can view my score and previous quiz scores :heavy_check_mark:                                                        |
+                                // | When I click clear highscores               | Then the higscores are cleared:heavy_check_mark: 
+
+                                // </br>
+                                // </br>
+
+
+                                // ## Credits
+                                // ---
+                                // :school: 
+                                // **University of Toronto Continuing Educationg
+                                // Full Stack Flex Program** - *Initial HTML and CSS*
+                                // :school:
+
+                                // :heart: 
+                                // *Students and Instructors At the UFT-FSF-2020-2021*
+                                // :heart:
+
+                                // </br>
+
+                                // ## License
+                                // ---
+
+                                // All rights reserved.
+
+                                // Licensed under the MIT license.
+                                // <br>
+                                // [![MIT License](https://img.shields.io/apm/l/atomic-design-ui.svg?)](https://github.com/tterb/atomic-design-ui/blob/master/LICENSEs)
+                                // </br>
+
+                                // :lock::lock::lock::lock:
+
+                                // _____
+                                // ____
+                                // ___`;
+
+
+
+
+
+

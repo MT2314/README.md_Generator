e9234d05db7b3c296565ccdc1f004f66798cfa23
MT2314 <39105763+MT2314@users.noreply.github.com>
2020-12-03 23:30:17 -0500

    README.md complete, badges added

--- README.md
+++ README.md
@@ -1,17 +1,53 @@
 
-    :part_alternation_mark:
+:part_alternation_mark:
+
     
-    # Mike
-    ---
-    <br />
+# sghdfhsghgfh
+---
+</br>
     
-    >Link to github repository sds
+>Link to github repository sfdgsdfgsdfg
     
-    <img src="assets/Images/websiteScreenshot.png" alt="alt text" width="600"/>
+<img src="assets/Images/websiteScreenshot.png" alt="alt text" width="600"/>
     
-    ## Job Requirements
-    ---
+## Description
+---
+> asfsadf
     
-    </br>
-    
-    > asd
+</br>
+</br>
+
+##Table of Contents
+---
+[Installation](#installation)
+[Usage](#usage-guide)
+[License](#license)
+[Contributing](#contributing)
+[Tests](#testing-instructions)
+
+### Installation
+---
+> czcvcsdvsd
+
+### Usage Guide
+---
+> fadsfasdf
+
+### Contributing
+---
+> asdfafasdf
+
+### Testing Instructions
+---
+> asddfc
+
+### License
+---
+> [![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)
+
+</br>
+:lock::lock::lock::lock:
+_____
+____
+___;
+
--- index.js
+++ index.js
@@ -13,147 +13,257 @@ const util = require('util');
 // fs writeFile is file system file which writes data to a file
 const writeFileAsync = util.promisify(fs.writeFile);
 
-const userInput1 = () =>
-    inquirer.prompt([
+// Util promisify converts callback based functions into promise based 
+// fs writeFile is file system file which writes data to a file
+const appendFileAsync = util.promisify(fs.appendFile);
 
-        // Project Name
-        {
-            type: "input",
-            name: "projectName",
-            message: "What is the name of your project?",
-        },
-        // Github Repo Link
-        {
-            type: "input",
-            name: "github",
-            message: "What is the github repository address?",
-        },
-        // Project Description
-        {
-            type: "input",
-            name: "description",
-            message: "How would you describe the project",
-        },
-    ]);
 
-const readMeGen = (user) =>
-    `
-    :part_alternation_mark:
-    
-    # ${user.projectName}
-    ---
-    <br />
-    
-    >Link to github repository ${user.github}
-    
-    <img src="assets/Images/websiteScreenshot.png" alt="alt text" width="600"/>
-    
-    ## Job Requirements
-    ---
-    
-    </br>
-    
-    > ${user.description}`;
+const init = function () {
 
-userInput1()
-    .then((user) => writeFileAsync("README.md", readMeGen(user)))
-    .then(() => {
-        // App's abilities breakdown
-        const table = () => {
+    const userInput = () =>
+        inquirer
+            .prompt([
 
-            inquirer.prompt([
-                // Do you want a Data Table?
-                // //
+                // Project Name
                 {
-                    type: "confirm",
-                    name: "table",
-                    message: "Do you want to build a table?",
+                    type: "input",
+                    name: "projectName",
+                    message: "What is the name of your project?",
                 },
+                // Github Repo Link
+                {
+                    type: "input",
+                    name: "github",
+                    message: "What is the github repository address?",
+                },
+                // Project Description
+                {
+                    type: "input",
+                    name: "description",
+                    message: "How would you describe the project",
+                },
+                // Installation Instructions
+                {
+                    type: "input",
+                    name: "installation",
+                    message: "How would the user instal dependencies?",
+                },
+                // Usage Instructions
+                {
+                    type: "input",
+                    name: "usage",
+                    message: "How does the user make use of the application",
+                },
+                // License
+                {
+                    type: "list",
+                    name: "license",
+                    message: "Which license are you using?",
+                    choices:["MIT", "Apache 2.0", "GPL 3.0"]
+                },
+                // Contributions
+                {
+                    type: "input",
+                    name: "contributing",
+                    message: "Has anyone contributed to this project?",
+                },
+                // Test
+                {
+                    type: "input",
+                    name: "testing",
+                    message: "How would the user test the application?",
+                },
+
             ]);
-            table()
-                .then((choice) => {
-                    const tableBuild = (choice) => {
-                        if (choice === true) {
-                            inquirer.prompt([
-                                // How many Columns?
-                                {
-                                    type: "input",
-                                    name: "columns",
-                                    message: "How many Columns",
-                                },
 
-                                // How many Rows?
-                                {
-                                    type: "input",
-                                    name: "rows",
-                                    message: "How many rows",
-                                },
-                            ])
-                        }
-                    };
-                });
-        };
-    });
-table()
-    .then(console.log("hi"))
+    const readMeGen = (user) =>
+
+    
+        `
+:part_alternation_mark:
+
+    
+# ${user.projectName}
+---
+</br>
+    
+>Link to github repository ${user.github}
+    
+<img src="assets/Images/websiteScreenshot.png" alt="alt text" width="600"/>
+    
+## Description
+---
+> ${user.description}
+    
+</br>
+</br>
+
+##Table of Contents
+---
+[Installation](#installation)
+[Usage](#usage-guide)
+[License](#license)
+[Contributing](#contributing)
+[Tests](#testing-instructions)
+
+### Installation
+---
+> ${user.installation}
+
+### Usage Guide
+---
+> ${user.usage}
+
+### Contributing
+---
+> ${user.contributing}
+
+### Testing Instructions
+---
+> ${user.testing}
+
+### License
+---
+> ${user.license}
+
+</br>
+:lock::lock::lock::lock:
+_____
+____
+___;
+
+`;
+
+    userInput()
+
+        .then(function (user) {
+           
+            if(user.license === "MIT"){
+                user.license = `[![MIT License](https://img.shields.io/apm/l/atomic-design-ui.svg?)](https://github.com/tterb/atomic-design-ui/blob/master/LICENSEs)` 
+            }
+            else if(user.license === "Apache 2.0"){
+                user.license = `[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)` 
+            }
+            else if(user.license === "Apache 2.0"){
+                user.license = `[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)` 
+            }
+            writeFileAsync("README.md", readMeGen(user))
+        })
+        .then(() => console.log('Successfully wrote to README.md'))
+        .catch((err) => console.log(err));
+
+};
+
+init();
+
+//     const userInputTable = () =>
+//         inquirer
+//             .prompt([
+//                 // Do you want a Data Table?
+//                 {
+//                     type: "confirm",
+//                     name: "table",
+//                     message: "Do you want to include User Story (2 column table objectives//solutions)?",
+//                 },
+//                 // Do you want a Data Table?
+//                 {
+//                     type: "input",
+//                     name: "rows",
+//                     message: "How many rows in your table?",
+//                     when: (user) => user.table === true,
+//                 },
+//             ]);
+
+//     const readMeTableGen = (user) =>
+//         `
 
 
 
-    .then(() => console.log('Successfully wrote to README.md'))
-    .catch((err) => console.log(err));
+
+//     userInputTable()
+//         .then(function (user) {
+//             appendFileAsync("README.md", readMeTableGen(user))
+//         })
+//         .then(() => console.log('Successfully wrote to README.md'))
+//         .catch((err) => console.log(err));
+//     // // table()
+//     //     .then((choice) => {
+//         //         const tableBuild = (choice) => {
+//             //             if (choice === true) {
+//                 //                 inquirer.prompt([
+//                     //                     // How many Columns?
+//                     //                     {
+//                         //                         type: "input",
+//                         //                         name: "columns",
+//                         //                         message: "How many Columns",
+//                         //                     },
+
+//                         //                     // How many Rows?
+//                                         //                     {
+//                                             //                         type: "input",
+//                                             //                         name: "rows",
+//                                             //                         message: "How many rows",
+//                                             //                     },
+//                                             //                 ])
+//                                             //             }
+//                                             //         };
+//                                             //         tableBuild();
 
 
 
-// const userInputTable = () => {
-    //     return prompt([{
-        //         ...question,
-        //         name: 'question'
-        //     }]).then(({ question }) => question)
-        // }
 
 
 
-        // .then((user) => tableBuild(user));
-        // const tableBuild()
-        //     .
-        //     if(user.table == true){
-
-            //  * /
-            //     // Project Name
-            //     {
-                //         type: "input",
-                //             name: "projectName",
-                //                 message: "What is the name of your project?",
-                //     },
-                //     // Github Repo Link
-                //     {
-                    //         type: "input",
-                    //             name: "github",
-                    //                 message: "What is the github repository address?",
-                    //     },
-                    //     // Project Description
-                    //     {
-                        //         type: "input",
-                        //             name: "description",
-                        //                 message: "How would you describe the project",
-                        //     },
+//                                             // const userInputTable = () => {
+//                                                 //     return prompt([{
+//                                                     //         ...question,
+//                                                     //         name: 'question'
+//                                                     //     }]).then(({ question }) => question)
+//                                                     // }
 
 
-                        //     // {
-                            //     //     type: "input",
-                            //     //     name: "location",
-                            //     //     message: "What is your location?",
-                            //     // },
-                            //     // {
-                                //     //     type: "input",
-                                //     //     name: "linkedin",
-                                //     //     message: "What is your linkedin?",
-                                //     // },
-                                // ]);
 
-                                //     const readMeGen = (user) =>
+//         // .then((user) => tableBuild(user));
+//         // const tableBuild()
+//         //     .
+//         //     if(user.table == true){
 
-                                //         `   
+//             //  * /
+//             //     // Project Name
+//             //     {
+//                 //         type: "input",
+//                 //             name: "projectName",
+//                 //                 message: "What is the name of your project?",
+//                 //     },
+//                 //     // Github Repo Link
+//                 //     {
+//                     //         type: "input",
+//                     //             name: "github",
+//                     //                 message: "What is the github repository address?",
+//                     //     },
+//                     //     // Project Description
+//                     //     {
+//                         //         type: "input",
+//                         //             name: "description",
+//                         //                 message: "How would you describe the project",
+//                         //     },
+
+
+//                         //     // {
+//                             //     //     type: "input",
+//                             //     //     name: "location",
+//                             //     //     message: "What is your location?",
+//                             //     // },
+//                             //     // {
+//                                 //     //     type: "input",
+//                                 //     //     name: "linkedin",
+//                                 //     //     message: "What is your linkedin?",
+//                                 //     // },
+//                                 // ]);
+
+//                                 //     const readMeGen = (user) =>
+
+//                                 //         `
 
                                 // </br>
                                 // | Desires                              | Solutions                                                                                       |
@@ -202,9 +312,3 @@ table()
                                 // _____
                                 // ____
                                 // ___`;
-
-
-
-
-
-
